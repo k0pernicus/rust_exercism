a2d9576a4287266e0fb829ab2b8fc37e157d640a
k0pernicus <antonin.carette@gmail.com>
2016-09-17 00:36:03 +0200

    Break up zeros with underscores

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -2,5 +2,5 @@ extern crate chrono;
 use chrono::*;
 
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
-    start_date + Duration::seconds(1000000000)
+    start_date + Duration::seconds(1_000_000_000)
 }
